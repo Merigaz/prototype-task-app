06a5081fa1e3ae6960e53ade078d4bfce7233cd0
Merigaz <davidpdavila@gmail.com>
2026-02-05 22:00:55 -0500

    starting to order css files

--- app/components/EisenhowerMatrix.tsx
+++ app/components/EisenhowerMatrix.tsx
@@ -1,18 +1,20 @@
 export default function EisenhowerMatrix() {
   return (
-    <div className="grid grid-cols-2 min-h-full">
+    <div className="grid grid-cols-2 min-h-full relative">
       <div className="matrix-v-line"></div>
       <div className="matrix-h-line"></div>
-      <div className=" text-black flex justify-center items-center">Hacer</div>
-      <div className="0 text-black flex justify-center items-center">
+      <section className=" text-black flex justify-center items-center">
+        Hacer
+      </section>
+      <section className=" text-black flex justify-center items-center">
         Decidir
-      </div>
-      <div className=" text-black flex justify-center items-center">
+      </section>
+      <section className=" text-black flex justify-center items-center">
         Delegar
-      </div>
-      <div className=" text-black flex justify-center items-center">
+      </section>
+      <section className=" text-black flex justify-center items-center">
         Eliminar
-      </div>
+      </section>
     </div>
   );
 }
